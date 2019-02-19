f519805f21604bd067fb6f6922fef693438e0006
thelockymichael <thelockymichael@gmail.com>
2019-02-19 15:23:49 +0200

    adsfa

---
+++ blog/src/actions/index.js
@@ -0,0 +1,14 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => {
+
+    return function (dispatch, getState) {
+
+        const promise = jsonPlaceholder.get('/posts');
+
+        return {
+            type: 'FETCH_POSTS',
+            payload: promise
+        };
+    }
+};
