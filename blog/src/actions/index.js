1a1f23484ebe6a3d8063e5cd48dd83dc06438901
thelockymichael <thelockymichael@gmail.com>
2019-02-24 17:11:15 +0200

    asdf

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,14 +1,41 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
 
-    return function (dispatch, getState) {
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)));
 
-        const promise = jsonPlaceholder.get('/posts');
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 
-        return {
-            type: 'FETCH_POSTS',
-            payload: promise
-        };
-    }
+    // await Promise.all(userIds.map(id => dispatch(fetchUser(id))))
+
+    console.log(userIds);
 };
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+};
+
+export const fetchUser = id => async dispatch => {
+    //const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+/*
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+*/
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from  './postReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers ({
-    replaceMe: () => 'hi there'
-
+    posts: postsReducer,
+    users: usersReducer
 });
